8c727aafe82045ea9a8588d78f3d9e9846d79bff
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-06-06 16:06:47 +0200

    Add form for user input and initial price calculation logic

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+/* Scrivere un programma che chieda all’utente:
+Il numero di chilometri da percorrere
+Età del passeggero Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65. */
+
+/* MILESTONE 1:
+Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console. */
+
+const ageEl = document.querySelector('.age');
+const distanceEl = document.querySelector('.km');
+const btnEl = document.querySelector('.btn');
+const inputEl = document.querySelector('.inputs');
+
+const priceForKm = 0.21;
+let price = distanceEl * priceForKm;
+
+
+inputEl.addEventListener('submit', function (e) {
+    if (ageEl < 18) {
+        const discountPercent = 20;
+        price = price * (1 - (discountPercent / 100));
+    } else if (ageEl > 65) {
+        const discountPercent = 40;
+        price = price * (1 - (discountPercent / 100))
+    }
+}
+)
+
+console.log(price)
+/* MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca. */
+
+/* MILESTONE 3:
+Ora che la logica è funzionante in pagina, possiamo andare a dedicarci allo stile, raffinando la parte di HTML e CSS in modo da renderla esteticamente gradevole.b */
